0fa7780a6ce1ba5cad26ef26f33e78ae3780b95f
NBin17 <83058853+dkhoa1709@users.noreply.github.com>
2022-11-02 10:34:14 +0700

    Complete UI Quan ly chuong nuoi, dan heo and giong heo

---
+++ FarmManagementSoftware/View/Windows/Quản lý đàn heo/LapLichWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FarmManagementSoftware.View.Windows.Quản_lý_đàn_heo
+{
+    /// <summary>
+    /// Interaction logic for LapLichWindow.xaml
+    /// </summary>
+    public partial class LapLichWindow : Window
+    {
+        public LapLichWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
