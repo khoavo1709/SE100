214f3c6d30bdc1da20013c7c68267e886040243b
Triệu Tuấn Tú <90883273+Vippro67@users.noreply.github.com>
2022-11-14 10:11:25 +0700

    Hoàn thiện 1 số giao diện còn lại.

---
+++ FarmManagementSoftware/View/Windows/Quản lý hàng hoá/TaoPhieuKiemKho.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FarmManagementSoftware.View.Windows.Quản_lý_hàng_hoá
+{
+    /// <summary>
+    /// Interaction logic for TaoPhieuKiemKho.xaml
+    /// </summary>
+    public partial class TaoPhieuKiemKho : Window
+    {
+        public TaoPhieuKiemKho()
+        {
+            InitializeComponent();
+        }
+    }
+}
