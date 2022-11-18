fb73dc9f00cc8a2de8c0969db496caa002a257fc
NBin17 <83058853+dkhoa1709@users.noreply.github.com>
2022-11-18 10:44:17 +0700

    tao file chuc vu model

--- FarmManagementSoftware/ViewModel/Quản lý nhân viên/ThemNhanVienVM.cs
+++ FarmManagementSoftware/ViewModel/Quản lý nhân viên/ThemNhanVienVM.cs
@@ -12,56 +12,56 @@ namespace FarmManagementSoftware.ViewModel
 {
     public class ThemNhanVienVM : BaseViewModel
     {
-        private NHANVIEN nHANVIEN;
+    //    private NHANVIEN nHANVIEN;
 
-        public ICommand ThemCommand { get; set; }
-        public ObservableCollection<CHUCVU> listChucVu { get; set; }
-        public CHUCVU chucvu { get; set; }
-        public NHANVIEN newNhanVien { get; set; }
-        public ThemNhanVienVM()
-        {
-            newNhanVien = new NHANVIEN();
-            listChucVu = new ObservableCollection<CHUCVU>();
-            chucvu = new CHUCVU();
-            LoadListChucVu();
-            ThemCommand = new RelayCommand<Window>((p) => { return true; }, p => { Them(p); });
+    //    public ICommand ThemCommand { get; set; }
+    //    public ObservableCollection<CHUCVU> listChucVu { get; set; }
+    //    public CHUCVU chucvu { get; set; }
+    //    public NHANVIEN newNhanVien { get; set; }
+    //    public ThemNhanVienVM()
+    //    {
+    //        newNhanVien = new NHANVIEN();
+    //        listChucVu = new ObservableCollection<CHUCVU>();
+    //        chucvu = new CHUCVU();
+    //        LoadListChucVu();
+    //        ThemCommand = new RelayCommand<Window>((p) => { return true; }, p => { Them(p); });
 
-        }
-        private void LoadListChucVu()
-        {
-            var listchucvu = DataProvider.Ins.DB.CHUCVUs.ToList();
-            foreach (var items in listchucvu)
-                listChucVu.Add(items);
+    //    }
+    //    private void LoadListChucVu()
+    //    {
+    //        var listchucvu = DataProvider.Ins.DB.CHUCVUs.ToList();
+    //        foreach (var items in listchucvu)
+    //            listChucVu.Add(items);
 
-        }
-        private void Them(Window p)
-        {
-            if (newNhanVien.HoTen == String.Empty || newNhanVien.HoTen == null)
-            {
-                MessageBox.Show("Vui lòng nhập họ tên ! ", "Thông báo!", MessageBoxButton.OK);
-                return;
-            }
-            if (newNhanVien.C_Username == String.Empty || newNhanVien.C_Username == null)
-            {
-                MessageBox.Show("Vui lòng nhập Tên đăng nhập! ", "Thông báo!", MessageBoxButton.OK);
-                return;
-            }
-            try { Convert.ToInt32(newNhanVien.HeSoLuong); }
-            catch
-            {
-                MessageBox.Show("Vui lòng đúng thông tin! ", "Thông báo!", MessageBoxButton.OK);
-                return;
-            }
-            newNhanVien.HoTen.ToString().Replace(" ", "");
-            newNhanVien.C_Username.ToString().Replace(" ", "");
+    //    }
+    //    private void Them(Window p)
+    //    {
+    //        if (newNhanVien.HoTen == String.Empty || newNhanVien.HoTen == null)
+    //        {
+    //            MessageBox.Show("Vui lòng nhập họ tên ! ", "Thông báo!", MessageBoxButton.OK);
+    //            return;
+    //        }
+    //        if (newNhanVien.C_Username == String.Empty || newNhanVien.C_Username == null)
+    //        {
+    //            MessageBox.Show("Vui lòng nhập Tên đăng nhập! ", "Thông báo!", MessageBoxButton.OK);
+    //            return;
+    //        }
+    //        try { Convert.ToInt32(newNhanVien.HeSoLuong); }
+    //        catch
+    //        {
+    //            MessageBox.Show("Vui lòng đúng thông tin! ", "Thông báo!", MessageBoxButton.OK);
+    //            return;
+    //        }
+    //        newNhanVien.HoTen.ToString().Replace(" ", "");
+    //        newNhanVien.C_Username.ToString().Replace(" ", "");
 
-            newNhanVien.MaChucVu = chucvu.MaChucVu;
-            newNhanVien.MaNhanVien = ("NV" + DataProvider.Ins.DB.NHANVIENs.Count().ToString()).Replace(" ", "");
-            DataProvider.Ins.DB.NHANVIENs.Add(newNhanVien);
-            DataProvider.Ins.DB.SaveChanges();
-            MessageBox.Show("Thêm nhân viên mới thành công! ", "Thông báo!", MessageBoxButton.OK);
-            newNhanVien = new NHANVIEN();
-            p.Close();
-        }
-    }
+    //        newNhanVien.MaChucVu = chucvu.MaChucVu;
+    //        newNhanVien.MaNhanVien = ("NV" + DataProvider.Ins.DB.NHANVIENs.Count().ToString()).Replace(" ", "");
+    //        DataProvider.Ins.DB.NHANVIENs.Add(newNhanVien);
+    //        DataProvider.Ins.DB.SaveChanges();
+    //        MessageBox.Show("Thêm nhân viên mới thành công! ", "Thông báo!", MessageBoxButton.OK);
+    //        newNhanVien = new NHANVIEN();
+    //        p.Close();
+    //    }
+    //}
 }
