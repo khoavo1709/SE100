9b0ec20ff3ec164f46c87473341e970ac4aa0c20
Triệu Tuấn Tú <tuantu120502@gmail.com>
2023-01-04 16:23:20 +0700

    sửa quản lý loại giống

--- FarmManagementSoftware/View/Windows/Quản lý giống heo/Quanlygiongheo.xaml.cs
+++ FarmManagementSoftware/View/Windows/Quản lý giống heo/Quanlygiongheo.xaml.cs
@@ -25,9 +25,8 @@ namespace FarmManagementSoftware.View.Windows.Quản_lý_giống_heo
         public Quanlygiongheo()
         {
             InitializeComponent();
-
-
-            reloadWithDataprovider();
+            Basegiongheo = DataProvider.Ins.DB.GIONGHEOs.ToList();
+            listviewHeo.ItemsSource = Basegiongheo;
         }
 
         /// <summary>
@@ -35,33 +34,50 @@ namespace FarmManagementSoftware.View.Windows.Quản_lý_giống_heo
         /// </summary>
         private void btn_ThemClick(object sender, RoutedEventArgs e)
         {
-            if (text1.Text == "")
+            /*if (text1.Text == "")
             {
                 MessageBox.Show("Chưa nhập mã loại heo.", "", MessageBoxButton.OK);
                 return;
-            }
+            }*/
             if (text2.Text == "" || text3.Text == "")
             {
                 MessageBox.Show("Chưa nhập đầy đủ thông tin.", "", MessageBoxButton.OK);
                 return;
             }
-            if (text1.Text != "")
+            if (text2.Text != "")
             {
-                if (Isexist(text1.Text) == false)
+                if (Isexist(text2.Text) == false)
                 {
                     var t = new GIONGHEO
                     {
-                        MaGiongHeo = text1.Text,
+                        MaGiongHeo = MaGiongHeo_CodeGenerate(),
                         TenGiongHeo = text2.Text,
                         MoTa = text3.Text
                     };
                     Add_ustomer(t);
+                    MessageBox.Show("Thêm thành công.", "", MessageBoxButton.OK);
+                    text2.Text = "";
+                    text3.Text = "";
                 }
                 else
                 {
                     MessageBox.Show("Đã tồn tại mã loại heo trên", "", MessageBoxButton.OK);
+                    text2.Text = "";
+                    text3.Text = "";
                 }
             }
+            reloadWithDataprovider();
+        }
+
+        string MaGiongHeo_CodeGenerate()
+        {
+            //create a function to generate random string
+            Random random = new Random();
+            string chars = "0123456789";
+            string result = "GH";
+            result += (new string(Enumerable.Repeat(chars, 14)
+              .Select(s => s[random.Next(s.Length)]).ToArray()));
+            return result;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -72,9 +88,12 @@ namespace FarmManagementSoftware.View.Windows.Quản_lý_giống_heo
         private void btnFix_Click(object sender, RoutedEventArgs e)
         {
             GIONGHEO giongheo = (GIONGHEO)listviewHeo.SelectedItem;
-            SuaGiongHeo sua = new SuaGiongHeo(giongheo);
-            sua.ShowDialog();
-            updating(sua.tranferCode());
+            if (giongheo != null)
+            {
+                SuaGiongHeo sua = new SuaGiongHeo(giongheo);
+                sua.ShowDialog();
+                updating(sua.tranferCode());
+            }
         }
 
         private void ListViewItem_PreviewMouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
@@ -100,7 +119,8 @@ namespace FarmManagementSoftware.View.Windows.Quản_lý_giống_heo
         private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
             GIONGHEO gIONGHEO = (GIONGHEO)listviewHeo.SelectedItem;
-            Delete(gIONGHEO);
+            if (gIONGHEO != null)
+                Delete(gIONGHEO);
         }
         /// <summary>
         /// methods
@@ -125,9 +145,9 @@ namespace FarmManagementSoftware.View.Windows.Quản_lý_giống_heo
         {
             try
             {
-                    DataProvider.Ins.DB.Entry(giongheo).State = System.Data.Entity.EntityState.Added;
-                    DataProvider.Ins.DB.SaveChanges();
-                    reloadWithDataprovider();
+                DataProvider.Ins.DB.Entry(giongheo).State = System.Data.Entity.EntityState.Added;
+                DataProvider.Ins.DB.SaveChanges();
+                reloadWithDataprovider();
             }
             catch (Exception)
             {
@@ -142,7 +162,7 @@ namespace FarmManagementSoftware.View.Windows.Quản_lý_giống_heo
             {
                 DataProvider.Ins.DB.GIONGHEOs.Remove(giongheo);
                 DataProvider.Ins.DB.SaveChanges();
-                reloadWithDataprovider();                
+                reloadWithDataprovider();
             }
             catch (Exception)
             {
@@ -153,7 +173,7 @@ namespace FarmManagementSoftware.View.Windows.Quản_lý_giống_heo
 
         private void Timkiem(string a)
         {
-            var t = DataProvider.Ins.DB.GIONGHEOs.Where(s => s.MaGiongHeo.Contains(a)).ToList();
+            var t = DataProvider.Ins.DB.GIONGHEOs.Where(s => s.TenGiongHeo.Contains(a)).ToList();
             if (t != null)
             {
                 Basegiongheo.Clear();
@@ -173,21 +193,26 @@ namespace FarmManagementSoftware.View.Windows.Quản_lý_giống_heo
 
         private bool Isexist(string check)
         {
-            var BaseGiongHeotemp = DataProvider.Ins.DB.GIONGHEOs.Where(s => s.MaGiongHeo.Contains(check)).ToList();
-            if (BaseGiongHeotemp != null)
+            var BaseGiongHeotemp = DataProvider.Ins.DB.GIONGHEOs.FirstOrDefault(s => s.TenGiongHeo.Contains(check));
+            if (BaseGiongHeotemp == null)
             {
                 return false;
             }
-            return true;
+            else
+            { return true; }
         }
 
         private void reloadWithDataprovider()
         {
             listviewHeo.ItemsSource = null;
+            Basegiongheo.Clear();
             Basegiongheo = DataProvider.Ins.DB.GIONGHEOs.ToList();
             listviewHeo.ItemsSource = Basegiongheo;
         }
 
-
+        private void Find_textbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Timkiem(Find_textbox.Text);
+        }
     }
 }
